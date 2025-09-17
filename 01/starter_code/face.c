b2588d739dde57b4945cba48769ef2789fa61df2
conrad <z5478184@ad.unsw.edu.au>
2025-09-17 13:33:55 +1000

    wk 1 tutorial

---
+++ 01/starter_code/face.c
@@ -0,0 +1,30 @@
+// face.c
+//
+// Written by Conrad Vernon (z7777777),
+// on Janaruary 2024
+//
+// The program prints two simple faces
+//
+// Face 1:
+//   ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+#include <stdio.h>
+
+int main(void) {
+    
+    printf("  ~ ~\n");
+    printf("  0 0\n");
+    printf("   o\n");
+    printf("  \\_/\n");
+
+    return 0;
+}
